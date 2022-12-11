a6ac1541c8f68e4995494357d67d1a234c5cc59d
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-11 21:37:48 +0100

    updated BookDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
@@ -1,5 +1,21 @@
 package ba.unsa.etf.rpr.dao;
 
-public interface BookDao {
-    
+import ba.unsa.etf.rpr.domain.Book;
+
+import java.util.List;
+
+public interface BookDao extends Dao<Book> {
+    /**
+     * return list of the books by the given author
+     * @param author - the author of the books
+     * @return list of books by the given author
+     */
+    public List<Book> searchByAuthor(String author);
+
+    /**
+     * return list of the books which have the given text in their name
+     * @param text - the text by which books are searched
+     * @return list of the books
+     */
+    public List<Book> searchByText(String text);
 }
