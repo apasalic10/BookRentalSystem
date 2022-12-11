c2e7046b8b59eceaba09dc256942168eef4859e8
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-11 21:59:05 +0100

    added MemberDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/MemberDao.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Member;
+
+import java.util.List;
+
+public interface MemberDao extends Dao<Member> {
+    /**
+     * return list of members with the given first name
+     * @param firstName - the first name by which members are searched
+     * @return list of members with the given first name
+     */
+    public List<Member> searchByFirstName(String firstName);
+
+    /**
+     * return list of members with the given last name
+     * @param lastName - the last name by which members are searched
+     * @return list of members with the given last name
+     */
+    public List<Member> searchByLastName(String lastName);
+}
