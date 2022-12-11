e45decf4cb68c63a159451d0ed0d6cccf043134b
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-11 23:13:22 +0100

    implemented constructor and searchByAuthor method in BookDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/BookDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDaoSQLImpl.java
@@ -3,20 +3,54 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.domain.Library;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 
 public class BookDaoSQLImpl implements BookDao{
 
     private Connection connection;
 
-    public BookDaoSQLImpl(){
+    public BookDaoSQLImpl() throws IOException{
+        FileReader reader = new FileReader("db.properties");
+        Properties p = new Properties();
+        p.load(reader);
+
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/" + p.getProperty("username") , p.getProperty("username"), p.getProperty("password"));
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Book> searchByAuthor(String author) {
-        return null;
+        List<Book> books = new LinkedList<>();
+
+        try{
+            PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM Books WHERE author = ?");
+            statement.setString(1,author);
+            ResultSet rs = statement.executeQuery();
+
+            while(rs.next()){
+                Book book = new Book();
+                book.setBookId(rs.getInt("book_id"));
+                book.setBookName(rs.getString("name"));
+                book.setBookLibrary(null);                             //implementirati poslije
+                book.setBookAuthor(rs.getString("author"));
+                books.add(book);
+            }
+
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return books;
     }
 
     @Override
