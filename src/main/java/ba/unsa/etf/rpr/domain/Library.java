ea7e48d6edcc3fb47a0bcd65fb37ea262274532e
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-11 20:48:54 +0100

    updated Library class

--- src/main/java/ba/unsa/etf/rpr/domain/Library.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Library.java
@@ -1,4 +1,55 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Library {
+   private int libraryId;
+   private String name;
+   private String location;
+
+    public int getLibraryId() {
+        return libraryId;
+    }
+
+    public void setLibraryId(int libraryId) {
+        this.libraryId = libraryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "libraryId=" + libraryId +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return libraryId == library.libraryId && name.equals(library.name) && location.equals(library.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(libraryId, name, location);
+    }
 }
