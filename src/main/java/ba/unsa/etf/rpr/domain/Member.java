5cca608e4b7206567b2706507d853f2750631ee0
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-11 20:45:01 +0100

    updated Member class

--- src/main/java/ba/unsa/etf/rpr/domain/Member.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Member.java
@@ -1,4 +1,95 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Member {
+    private int memberId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberId=" + memberId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return memberId == member.memberId && firstName.equals(member.firstName) && lastName.equals(member.lastName) && username.equals(member.username) && password.equals(member.password) && email.equals(member.email) && phoneNumber.equals(member.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberId, firstName, lastName, username, password, email, phoneNumber);
+    }
 }
