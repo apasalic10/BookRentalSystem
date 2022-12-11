5ea754c3e14db514dc38d9437ba9b8110a4b191d
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-11 21:51:24 +0100

    added LibraryDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/LibraryDao.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Library;
+
+import java.util.List;
+
+public interface LibraryDao extends Dao<Library> {
+    /**
+     * return library with the given name
+     * @param name of the library
+     * @return library with the given name
+     */
+    public Library getByName(String name);
+
+    /**
+     * return list of libraries that are in the given location
+     * @param location - the location where the libraries are located
+     * @return list of libraries that are in the given location
+     */
+    public List<Library> searchByLocation(String location);
+}
