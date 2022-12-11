fd408b1773bedd3c8b436f670a5341b66e6f7f5c
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-11 21:21:14 +0100

    added bookAuthor attribute in Book class

--- src/main/java/ba/unsa/etf/rpr/domain/Book.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Book.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class Book {
     private int bookId;
     private String bookName;
+    private String bookAuthor;
     private Library bookLibrary;
 
     public int getBookId() {
@@ -23,6 +24,14 @@ public class Book {
         this.bookName = bookName;
     }
 
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
     public Library getBookLibrary() {
         return bookLibrary;
     }
@@ -36,6 +45,7 @@ public class Book {
         return "Book{" +
                 "bookId=" + bookId +
                 ", bookName='" + bookName + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
                 ", bookLibrary=" + bookLibrary +
                 '}';
     }
@@ -45,11 +55,11 @@ public class Book {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return bookId == book.bookId && bookName.equals(book.bookName) && bookLibrary.equals(book.bookLibrary);
+        return bookId == book.bookId && bookName.equals(book.bookName) && bookAuthor.equals(book.bookAuthor) && bookLibrary.equals(book.bookLibrary);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(bookId, bookName, bookLibrary);
+        return Objects.hash(bookId, bookName, bookAuthor, bookLibrary);
     }
 }
