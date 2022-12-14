17e47e0211474656fa3ba8d6558c34cead6c1a27
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-14 19:44:07 +0100

    implemented getByName method

--- src/main/java/ba/unsa/etf/rpr/dao/LibraryDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/LibraryDaoSQLImpl.java
@@ -1,13 +1,16 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.Library;
+
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 
-public class LibraryDaoSQLImpl {
+public class LibraryDaoSQLImpl implements LibraryDao {
 
     private Connection connection;
 
@@ -21,5 +24,58 @@ public class LibraryDaoSQLImpl {
         }catch (SQLException e){
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public Library getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Library add(Library item) {
+        return null;
+    }
+
+    @Override
+    public Library update(Library item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Library> getAll() {
+        return null;
+    }
+
+    @Override
+    public Library getByName(String name) {
+
+        try {
+            PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM Libraries WHERE name = ?");
+            statement.setString(1,name);
+            ResultSet rs = statement.executeQuery();
+
+            if(rs.next()){
+                Library lib = new Library();
+                lib.setLibraryId(rs.getInt("library_id"));
+                lib.setName(rs.getString("name"));
+                lib.setLocation(rs.getString("location"));
+                return lib;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Library> searchByLocation(String location) {
+        return null;
     }
 }
