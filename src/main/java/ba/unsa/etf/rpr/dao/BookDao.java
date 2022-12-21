a5861a3fa4aa06ef85b595731c6b06253ecc9e69
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-21 19:21:26 +0100

    fixed throws in BookDao and Dao classes

--- src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.domain.Library;
+import ba.unsa.etf.rpr.exceptions.BookException;
 
 import java.util.List;
 
@@ -11,25 +12,25 @@ public interface BookDao extends Dao<Book> {
      * @param author - the author of the books
      * @return list of books by the given author
      */
-    public List<Book> searchByAuthor(String author);
+    public List<Book> searchByAuthor(String author) throws BookException;
 
     /**
      * return list of the books which have the given text in their name
      * @param text - the text by which books are searched
      * @return list of the books  which have the given text in their name
      */
-    public List<Book> searchByText(String text);
+    public List<Book> searchByText(String text) throws BookException;
 
     /**
      * return list of books for the given library
      * @param library - the library where books are searched
      * @return list of books for the given library
      */
-    List<Book> searchByLibrary(Library library);
+    List<Book> searchByLibrary(Library library) throws BookException;
 
     /**
      * return list of all available books
      * @return list of available books
      */
-    List<Book> searchAllAvailable();
+    List<Book> searchAllAvailable() throws BookException;
 }
--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.BookException;
+
 import java.util.List;
 
 /**
@@ -13,31 +15,31 @@ public interface Dao<Type> {
      * @param id primary key of entity in database
      * @return entity from database with the given id
      */
-    public Type getById(int id);
+    public Type getById(int id) throws BookException;
 
     /**
      * add entity into database
      * @param item which will be saved to the database
      * @return saved item
      */
-    public Type add(Type item);
+    public Type add(Type item) throws BookException;
 
     /**
      * update entity in database
      * @param item been to be updated. id must be populated.
      * @return updated version item
      */
-    public Type update(Type item);
+    public Type update(Type item) throws BookException;
 
     /**
      * delete item with the given id
      * @param id primary key of entity which is deleted
      */
-    public void delete(int id);
+    public void delete(int id) throws BookException;
 
     /**
      * return list with all entities from database. WARNING: Very slow operation because it reads all records.
      * @return list of all entities
      */
-    public List<Type> getAll();
+    public List<Type> getAll() throws BookException;
 }
