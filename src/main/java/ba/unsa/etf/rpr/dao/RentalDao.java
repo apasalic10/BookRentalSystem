d1fe71f4d6482688a911c7d5720972d20e682ddf
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-21 19:54:51 +0100

    fixed throws in all methods

--- src/main/java/ba/unsa/etf/rpr/dao/RentalDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RentalDao.java
@@ -3,8 +3,8 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.domain.Member;
 import ba.unsa.etf.rpr.domain.Rental;
+import ba.unsa.etf.rpr.exceptions.BookException;
 
-import java.util.Date;
 import java.util.List;
 
 public interface RentalDao extends Dao<Rental> {
@@ -13,19 +13,20 @@ public interface RentalDao extends Dao<Rental> {
      * @param book - the book from which rents are requested
      * @return list of rentals for the given book
      */
-    public List<Rental> searchByBook(Book book);
+    public List<Rental> searchByBook(Book book) throws BookException;
 
     /**
      * return rent list of the given member
      * @param member - the member from whom rents are requested
      * @return list of rentals for the given member
      */
-    public List<Rental> searchByMember(Member member);
+    public List<Rental> searchByMember(Member member) throws BookException;
 
     /**
      * rent list for the given date
      * @param date - the date by which rents are requested
      * @return list of rentals for the given date
      */
-    public List<Rental> searchByDate (Date date);
+    public List<Rental> searchByDate (java.sql.Date date) throws BookException;
+
 }
