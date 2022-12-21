c2b4427b4f3e43e519e645161078f0beb7821107
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-21 19:45:58 +0100

    refactored MemberDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/MemberDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MemberDaoSQLImpl.java
@@ -1,205 +1,99 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.domain.Library;
 import ba.unsa.etf.rpr.domain.Member;
+import ba.unsa.etf.rpr.exceptions.BookException;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Properties;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class MemberDaoSQLImpl implements MemberDao{
+public class MemberDaoSQLImpl extends AbstractDao<Member> implements MemberDao{
 
-    private Connection connection;
 
-    public MemberDaoSQLImpl() throws IOException {
-        FileReader reader = new FileReader("db.properties");
-        Properties p = new Properties();
-        p.load(reader);
-
-        try {
-            this.connection = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/" + p.getProperty("username") , p.getProperty("username"), p.getProperty("password"));
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+    MemberDaoSQLImpl() {
+        super("Members");
     }
 
 
     @Override
-    public Member getById(int id) {
-
+    public Member row2object(ResultSet rs) throws BookException {
         try{
-            PreparedStatement statement = this.connection.prepareStatement("SELECT  * FROM Members WHERE member_id = ? ");
-            statement.setInt(1,id);
-            ResultSet rs = statement.executeQuery();
+            Member mem = new Member();
+            mem.setId(rs.getInt(1));
+            mem.setFirstName(rs.getString(2));
+            mem.setLastName(rs.getString(3));
+            mem.setUsername(rs.getString(4));
+            mem.setPassword(rs.getString(5));
+            mem.setEmail(rs.getString(6));
+            mem.setPhoneNumber(rs.getString(7));
 
-            if(rs.next()){
-                Member mem = new Member();
-                mem.setMemberId(rs.getInt(1));
-                mem.setFirstName(rs.getString(2));
-                mem.setLastName(rs.getString(3));
-                mem.setUsername(rs.getString(4));
-                mem.setPassword(rs.getString(5));
-                mem.setEmail(rs.getString(6));
-                mem.setPhoneNumber(rs.getString(7));
-                return mem;
-            }
-            else{
-                System.out.println("Object not found!");
-            }
-
-            rs.close();
+            return mem;
         }catch (SQLException e){
-            e.printStackTrace();
+            throw new BookException(e.getMessage(),e);
         }
-        return null;
+
     }
 
     @Override
-    public Member add(Member item) {
+    public Map<String, Object> object2row(Member object) {
+        Map<String, Object> row = new TreeMap<>();
 
-        try {
-            PreparedStatement statement = this.connection.prepareStatement("INSERT INTO Members(first_name,last_name,username,password,email,phone_number) VALUES(?,?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1,item.getFirstName());
-            statement.setString(2,item.getLastName());
-            statement.setString(3,item.getUsername());
-            statement.setString(4,item.getPassword());
-            statement.setString(5,item.getEmail());
-            statement.setString(6,item.getPhoneNumber());
+        row.put("id", object.getId());
+        row.put("first_name", object.getFirstName());
+        row.put("last_id",object.getLastName());
+        row.put("username",object.getUsername());
+        row.put("password",object.getPassword());
+        row.put("email",object.getEmail());
+        row.put("phone_number",object.getPhoneNumber());
 
-
-            statement.executeUpdate();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-
-        return item;
+        return row;
     }
 
-    @Override
-    public Member update(Member item) {
-
-        String query = "UPDATE Libraries SET member_id = " + item.getMemberId() + ", first_name = " + item.getFirstName() +
-                ", last_name = " + item.getLastName() + ", username = " + item.getUsername() + ", password = " + item.getPassword() +
-                ", email = " + item.getEmail() + ", phone_number = " + item.getPhoneNumber()  + " WHERE member_id = " + item.getMemberId();
-
-        try{
-            PreparedStatement statement = this.connection.prepareStatement(query);
-            statement.executeUpdate();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-
-        return item;
-    }
 
     @Override
-    public void delete(int id) {
-        String delete = "DELETE FROM Members WHERE member_id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(delete, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, id);
-            stmt.executeUpdate();
-
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<Member> getAll() {
-
-        String query = "SELECT * FROM Members";
-        List<Member> members = new LinkedList<>();
-
-        try {
-            PreparedStatement statement = this.connection.prepareStatement(query);
-            ResultSet rs = statement.executeQuery();
-
-            while(rs.next()){
-                Member mem = new Member();
-                mem.setMemberId(rs.getInt(1));
-                mem.setFirstName(rs.getString(2));
-                mem.setLastName(rs.getString(3));
-                mem.setUsername(rs.getString(4));
-                mem.setPassword(rs.getString(5));
-                mem.setEmail(rs.getString(6));
-                mem.setPhoneNumber(rs.getString(7));
-
-                members.add(mem);
-            }
-
-            rs.close();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-
-        return  members;
-    }
-
-    @Override
-    public List<Member> searchByFirstName(String firstName) {
+    public List<Member> searchByFirstName(String firstName) throws BookException {
         List<Member> members = new LinkedList<>();
 
         try{
-            PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM Members WHERE first_name = ?");
+            PreparedStatement statement = this.getConnection().prepareStatement("SELECT * FROM Members WHERE first_name = ?");
             statement.setString(1,firstName);
             ResultSet rs = statement.executeQuery();
 
             while(rs.next()){
-                Member mem = new Member();
-                mem.setMemberId(rs.getInt(1));
-                mem.setFirstName(rs.getString(2));
-                mem.setLastName(rs.getString(3));
-                mem.setUsername(rs.getString(4));
-                mem.setPassword(rs.getString(5));
-                mem.setEmail(rs.getString(6));
-                mem.setPhoneNumber(rs.getString(7));
-
-                members.add(mem);
+               members.add(row2object(rs));
             }
 
             rs.close();
         }
         catch (SQLException e){
-            e.printStackTrace();
+            throw new BookException(e.getMessage(),e);
         }
 
         return members;
     }
 
     @Override
-    public List<Member> searchByLastName(String lastName) {
+    public List<Member> searchByLastName(String lastName) throws BookException {
 
         List<Member> members = new LinkedList<>();
 
         try{
-            PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM Members WHERE last_name = ?");
+            PreparedStatement statement = this.getConnection().prepareStatement("SELECT * FROM Members WHERE last_name = ?");
             statement.setString(1,lastName);
             ResultSet rs = statement.executeQuery();
 
             while(rs.next()){
-                Member mem = new Member();
-                mem.setMemberId(rs.getInt(1));
-                mem.setFirstName(rs.getString(2));
-                mem.setLastName(rs.getString(3));
-                mem.setUsername(rs.getString(4));
-                mem.setPassword(rs.getString(5));
-                mem.setEmail(rs.getString(6));
-                mem.setPhoneNumber(rs.getString(7));
-
-                members.add(mem);
+                members.add(row2object(rs));
             }
 
             rs.close();
         }
         catch (SQLException e){
-            e.printStackTrace();
+            throw new BookException(e.getMessage(),e);
         }
 
         return members;
