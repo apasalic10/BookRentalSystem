6e0d1c9739c586ce49bf94e539bc22248ac3806e
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-21 19:35:29 +0100

    fixed throws in all methods

--- src/main/java/ba/unsa/etf/rpr/dao/LibraryDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/LibraryDao.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Library;
+import ba.unsa.etf.rpr.exceptions.BookException;
 
 import java.util.List;
 
@@ -10,12 +11,12 @@ public interface LibraryDao extends Dao<Library> {
      * @param name of the library
      * @return library with the given name
      */
-    public Library getByName(String name);
+    public Library getByName(String name) throws BookException;
 
     /**
      * return list of libraries that are in the given location
      * @param location - the location where the libraries are located
      * @return list of libraries that are in the given location
      */
-    public List<Library> searchByLocation(String location);
+    public List<Library> searchByLocation(String location) throws BookException;
 }
