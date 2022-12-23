fd1f0b92d4280da1e8f396cf1c9c9bada36c7551
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-12-23 17:17:40 +0100

    added login.fxml file for loginScreen

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,10 +1,26 @@
 package ba.unsa.etf.rpr;
 
 
-public class App
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class App extends Application
 {
     public static void main(String[] args) {
+        launch(args);
+    }
 
-        System.out.println("Hello World!");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
     }
 }
