7875877b65918914e39e8612ec46bf29b3bad6b0
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-01-06 13:46:04 +0100

    added initialize method in LoginController

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -9,6 +11,18 @@ public class LoginController {
     public PasswordField passwordId;
     public Button loginButton;
     public Button signupButton;
+    public Label neispravanpassId;
 
 
+    @FXML
+    public void initialize(){
+        passwordId.textProperty().addListener((obs,oldValue,newValue)->{
+            if(newValue.length() >= 5){
+                neispravanpassId.setText("");
+            }
+            else{
+                neispravanpassId.setText("Password must have at least five characters!");
+            }
+        });
+    }
 }
