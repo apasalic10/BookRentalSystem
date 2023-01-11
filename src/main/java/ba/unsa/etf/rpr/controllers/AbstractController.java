1c19baae8d9ac4b193359d81ec2171b508f7affa
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-01-11 18:17:49 +0100

    crated AbstractController class and implemented switchScreen method

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AbstractController.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class AbstractController {
+    static void switchScreen(Stage previousStage, String newStageName, String stageTitle) throws IOException {
+        FXMLLoader root = new FXMLLoader(AbstractController.class.getResource("/fxml/" + newStageName));
+        Stage newStage = new Stage();
+        newStage.setTitle(stageTitle);
+        newStage.getIcons().add(new Image("/img/LoginScreen/redBookIcon.png"));
+        newStage.setResizable(false);
+        newStage.setScene(new Scene(root.load(),USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        previousStage.hide();
+        newStage.show();
+    }
+}
