c61f7a4be065de1c697b6571b3c967a1a20fb1c2
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-01-11 22:43:08 +0100

    implemented initialize and signUpClick method

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,18 +1,8 @@
 package ba.unsa.etf.rpr;
 
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
-
 public class App {
     public static void main(String[] args) {
-
     }
 
 
--- src/main/java/ba/unsa/etf/rpr/controllers/SignUpController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignUpController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -8,10 +10,39 @@ import java.io.IOException;
 
 public class SignUpController {
     public GridPane signUpScreen;
+    public TextField firstnameId;
+    public TextField sign_usernameId;
+    public TextField sign_emailId;
+    public TextField lastnameId;
+    public PasswordField sign_passwordId;
+    public Button signUpButtonId;
+    public Button loginButtonId;
+    public Label neispravanpassId;
+    public TextField phonenumberId;
 
-    public void cancelClick(ActionEvent actionEvent) throws IOException {
+    @FXML
+    public void initialize(){
+        sign_passwordId.textProperty().addListener((obs,oldValue,newValue)->{
+            if(newValue.trim().length() >= 5){
+                neispravanpassId.setText("");
+            }
+            else{
+                neispravanpassId.setText("Use at least 5 charachters!");
+            }
+        });
+    }
+
+    public void loginClick(ActionEvent actionEvent) throws IOException {
         Stage ns = (Stage) signUpScreen.getScene().getWindow();
         AbstractController.switchScreen(ns,"login.fxml","Login");
     }
 
+    public void signUpClick(ActionEvent actionEvent) {
+        if(firstnameId.getText().isEmpty() || lastnameId.getText().isEmpty() || sign_usernameId.getText().isEmpty() || sign_passwordId.getText().isEmpty() || sign_emailId.getText().isEmpty() || phonenumberId.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Not all fields have been entered!");
+            alert.setHeaderText("Error");
+            alert.show();
+        }
+    }
 }
