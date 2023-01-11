367903f01554bf328c81bc55d07c3a356eac7091
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-01-11 18:25:34 +0100

    AppFx class added and modified class App

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -10,19 +10,10 @@ import javafx.stage.Stage;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
-public class App extends Application
-{
+public class App {
     public static void main(String[] args) {
-        launch(args);
+
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-        primaryStage.setTitle("Login");
-        primaryStage.getIcons().add(new Image("/img/LoginScreen/redBookIcon.png"));
-        primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        primaryStage.show();
-    }
+
 }
