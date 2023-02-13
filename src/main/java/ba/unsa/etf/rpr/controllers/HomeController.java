5d1448d8efb9fee18950d94390eaf98688c12598
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-02-13 20:09:01 +0100

    implemented rentBookClick method

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeController {
+    public Button rentBookButton;
+
+    public void rentBookClick(ActionEvent actionEvent) throws IOException {
+        Stage ns = (Stage) rentBookButton.getScene().getWindow();
+        AbstractController.switchScreen(ns,"rentScreen.fxml","Rent book");
+    }
+}
