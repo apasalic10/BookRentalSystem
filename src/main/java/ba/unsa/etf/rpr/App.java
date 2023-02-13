d129d1387f9b10d93f764e500591e5ef405b71dc
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-02-13 23:32:57 +0100

    tested method in app class

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,10 +1,17 @@
 package ba.unsa.etf.rpr;
 
 
-import java.sql.*;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Member;
+import ba.unsa.etf.rpr.domain.Rental;
+import ba.unsa.etf.rpr.exceptions.BookException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BookException {
     }
 
 
