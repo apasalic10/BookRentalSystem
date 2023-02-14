9d4ef6acd0f112f988411c01196f95d047b868f6
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-02-14 23:58:33 +0100

    created controller class for admin screen

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class AdminController {
+    //components
+    public Button membersButton;
+
+    public void membersButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void booksButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void librariesButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void logOutClick(ActionEvent actionEvent) {
+    }
+
+    public void rentalsButtonClick(ActionEvent actionEvent) {
+    }
+}
