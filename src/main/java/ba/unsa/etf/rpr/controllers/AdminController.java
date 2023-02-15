ab40b2ec07e7674d4c9404547c7c8b895a3424e0
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-02-15 14:28:19 +0100

    updated logOutClick method

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
@@ -1,10 +1,13 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class AdminController {
     //components
@@ -27,7 +30,14 @@ public class AdminController {
 
     public void logOutClick(ActionEvent actionEvent) throws IOException {
         Stage ns = (Stage) membersButton.getScene().getWindow();
-        AbstractController.switchScreen(ns,"login.fxml","Login");
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Warning");
+        alert.setContentText("Do You want to log out?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get().getButtonData().isDefaultButton()){
+            AbstractController.switchScreen(ns,"login.fxml","Login");
+        }
     }
 
     public void rentalsButtonClick(ActionEvent actionEvent) throws IOException {
