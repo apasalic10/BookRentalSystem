576eb7b5eabe85241fe52815344ae4b5c4f0b77b
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-02-15 12:10:40 +0100

    updated controller for admin screen

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
@@ -2,21 +2,30 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminController {
     //components
     public Button membersButton;
 
-    public void membersButtonClick(ActionEvent actionEvent) {
+    public void membersButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage ns = (Stage) membersButton.getScene().getWindow();
+        AbstractController.switchScene(ns,"adminMembersScreen.fxml","Members");
     }
 
-    public void booksButtonClick(ActionEvent actionEvent) {
+    public void booksButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage ns = (Stage) membersButton.getScene().getWindow();
+        AbstractController.switchScene(ns,"adminBook.fxml","Books");
     }
 
     public void librariesButtonClick(ActionEvent actionEvent) {
     }
 
-    public void logOutClick(ActionEvent actionEvent) {
+    public void logOutClick(ActionEvent actionEvent) throws IOException {
+        Stage ns = (Stage) membersButton.getScene().getWindow();
+        AbstractController.switchScreen(ns,"login.fxml","Login");
     }
 
     public void rentalsButtonClick(ActionEvent actionEvent) {
