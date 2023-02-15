15fca5a0405bb2f7501cdceaebf374db8437898e
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-02-15 12:15:31 +0100

    fixed deleteCLick method

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminMembersController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminMembersController.java
@@ -45,17 +45,6 @@ public class AdminMembersController {
         refreshMembers();
 
 
-        /*memberList.getSelectionModel().selectedItemProperty().addListener((obs,oldSelection,newSelection) -> {
-            if(newSelection != null){
-                Member member = (Member) newSelection;
-                firstNameId.setText(member.getFirstName());
-                lastNameId.setText(member.getLastName());
-                usernameId.setText(member.getUsername());
-                emailId.setText(member.getEmail());
-                phoneNumberId.setText(member.getPhoneNumber());
-            }
-        });*/
-
     }
 
     private void refreshMembers() {
@@ -72,10 +61,18 @@ public class AdminMembersController {
         Member member = (Member) memberList.getSelectionModel().getSelectedItem();
 
         if(member != null){
-            for(Rental r : rentalManager.searchByMember(member)){
-                rentalManager.delete(r.getId());
+            if(LoginController.getUsername().equals(member.getUsername())){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("You can't delete admin account!");
+                alert.setHeaderText(null);
+                alert.show();
             }
-            memberManager.delete(member.getId());
+            else{
+                for(Rental r : rentalManager.searchByMember(member)){
+                    rentalManager.delete(r.getId());
+                }
+                memberManager.delete(member.getId());
+            }
         }
 
         refreshMembers();
